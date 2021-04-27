6ef453a4a15a9f5ad316a424483198b056fbf637
Ekam Sidhu <ekamsinghsidhu@yahoo.com>
2021-04-27 12:33:21 -0700

    added roll

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,15 +6,19 @@ onready var animationState = animationTree.get("parameters/playback")
 
 const ACCELERATION = 400
 const MAX_SPEED = 80
+const ROLL_SPEED = 100
 const FRICTION = 400
-var velocity = Vector2.ZERO
 
 enum {
 	MOVE,
 	ROLL,
 	ATTACK
 }
+
 var state = MOVE
+var velocity = Vector2.ZERO
+var roll_vector = Vector2.RIGHT
+
 func _ready():
 	animationTree.active = true
 
@@ -24,10 +28,10 @@ func _physics_process(delta):
 			move_state(delta)
 			
 		ROLL:
-			pass
+			roll_state(delta)
 			
 		ATTACK:
-			attack_state(delta)
+			attack_state()
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
@@ -37,25 +41,40 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
+		
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
-	velocity = move_and_slide(velocity)
+		
+	move()
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 		
+func roll_state(delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
 	
-		
-func attack_state(delta):
+func attack_state():
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
+	
+func move():
+	velocity = move_and_slide(velocity)
+	
+func roll_animation_finished():
+	velocity = velocity/1.25
+	state = MOVE
 	
 func attack_animation_finished():
 	state = MOVE
